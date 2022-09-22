f869612fa35080a3c2820442b2e540563743a1ba
Ahmed massoud <ahmedmassoud@MacBook-Pro-von-Ahmed.local>
2022-09-22 19:25:53 +0200

    inital commit quiz context

---
+++ src/Components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const Loading = () => {
+  return (
+    <main>
+      <div className="loading"></div>
+    </main>
+  );
+};
+
+export default Loading;
